2f5f16cc9244bb59131e0894116a4e7b85f2e408
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-08 20:14:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getDatabase, ref, onValue, push, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 // Firebase Console -> Project Settings -> Config values paste pannunga
+
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "PASTE_PROJECT",
-  storageBucket: "PASTE_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyCwcgXc8fRAQTM36Tjwnx94B1vbCQ19mK8",
+  authDomain: "college-app-6feb1.firebaseapp.com",
+  projectId: "college-app-6feb1",
+  storageBucket: "college-app-6feb1.firebasestorage.app",
+  messagingSenderId: "643620831036",
+  appId: "1:643620831036:web:309d57c55ec201cb0ee839",
+  measurementId: "G-7Z6HXF5KM8"
 };
 
 let db = null;
