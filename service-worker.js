1abac15ba2f1c6be0a1d8d0c3ce4ee11d518e983
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-08 20:11:43 +0530

    Update cached files in service worker installation

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
 const CACHE='smart-college-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','css/style.css','js/app.js','js/firebase.js','manifest.json'])))});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','style.css','app.js','firebase.js','manifest.json'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
